c4cd07f24468955f3c57daa77883c25cfd2b8fe1
nohjonghyeok <sofaissofa@icloud.com>
2020-11-08 11:24:28 +0900

    changed constructor

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -4,9 +4,9 @@ class QuizBrain {
   int _questionNumber = 0;
 
   List <Question> _questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
-    Question(q: 'A slug\'s blood is green.', a: false),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', false),
   ];
 
   String getQuestionText() {
